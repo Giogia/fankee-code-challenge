61b17cae79c0f1f9ea114c340dd34346d54537eb
Giovanni Tommasi <giovannitommasi95@gmail.com>
2024-03-20 22:34:06 +0100

    Added Input Component

---
+++ components/input/Input.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames'
+
+import { InputProps } from './Input.props'
+import { Type } from './Input.types'
+
+/**
+ * UI component for handling user input data
+ */
+export const Input = ({
+	type,
+	placeholder,
+	value,
+	onChange
+}: InputProps) => {
+
+	return (
+		<input
+			type={type}
+			className={classNames([
+				'inline-flex w-full',
+				'border-2 border-gray-300',
+				'text-sm text-gray-800',
+				'bg-transparent h-10 px-3 rounded-lg',
+				'transition duration-250 ease-in-out',
+			])}
+			placeholder={placeholder}
+			value={value}
+			onChange={e => onChange?.(e?.target?.value)}
+		/>
+	)
+}
+
+Input.defaultProps = {
+	type: Type.Text
+}
+
+Input.Type = Type
---
+++ components/input/Input.types.tsx
@@ -0,0 +1,4 @@
+export enum Type {
+    Text = 'text',
+    Email = 'email',
+}
