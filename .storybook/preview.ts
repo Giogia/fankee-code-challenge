db048c0a24558a93ff0e5a19da9802930e338806
Giovanni Tommasi <giovannitommasi95@gmail.com>
2024-03-20 17:25:40 +0100

    Storybook theming

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,23 @@
 import type { Preview } from "@storybook/react"
 
+import { themes } from '@storybook/theming'
+
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      grid: {
+        disable: true
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    docs: {
+      theme: themes.dark,
     },
   },
 }
---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Fankee',
+    brandUrl: 'https://fankee.co',
+    brandImage: 'fankee.svg',
+    brandTarget: '_self',
+})
