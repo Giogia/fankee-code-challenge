bdcf888ccd70ccf447d3dcf6e15e593ad346e266
Giovanni Tommasi <giovannitommasi95@gmail.com>
2024-03-21 13:09:39 +0100

    Added Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
-  )
+
+import Link from 'next/link'
+
+import Fankee from '../public/fankee.svg'
+
+export default function Home() {    
+   return (
+      <main className="flex min-h-screen flex-col items-center justify-center gap-8 animate-in">
+         <Fankee />
+         <Link href="/login">Login</Link>
+      </main>
+   )
 }
