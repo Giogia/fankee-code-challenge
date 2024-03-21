d568d508337223b589c856062101aa5ed058f034
Giovanni Tommasi <giovannitommasi95@gmail.com>
2024-03-21 14:40:32 +0100

    Added Page Tests (#21)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import Image from 'next/image'
 import Link from 'next/link'
 
 import Fankee from '../public/fankee.svg'
@@ -6,7 +7,7 @@ import Fankee from '../public/fankee.svg'
 export default function Home() {    
    return (
       <main className="flex min-h-screen flex-col items-center justify-center gap-8 animate-in">
-         <Fankee />
+         <Image priority src={Fankee} alt='Fankee Logo' />
          <Link href="/login">Login</Link>
       </main>
    )
