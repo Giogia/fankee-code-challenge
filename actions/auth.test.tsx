6379933ba634c28a498e247294e1c3076d8bc316
Giovanni Tommasi <giovannitommasi95@gmail.com>
2024-03-21 23:05:55 +0100

    Added Login Error Handling

--- actions/auth.test.tsx
+++ actions/auth.test.tsx
@@ -16,29 +16,28 @@ jest.mock('../utils/supabase/server', () => ({
 
 const mockRedirect = jest.spyOn(nextNavigation, 'redirect')
 
-describe('Auth Module', () => {
-   it('redirects to error page on signIn failure', async () => {
-       
-      const formData = new FormData()
-      formData.append('email', 'test@example.com')
+const fixedTimestamp = new Date('1995-12-12T13:05:00Z').getTime()
 
-      await signIn(formData)
-      expect(mockRedirect).toHaveBeenCalledWith('/login?error=Could not authenticate user: Test error')
+describe('Auth Module', () => {
+
+   beforeAll(() => jest.spyOn(Date, 'now').mockImplementation(() => fixedTimestamp))
+   afterAll(() => jest.spyOn(Date, 'now').mockRestore())
+    
+   it('redirects to error page on signIn failure', async () => {
+
+      await signIn('test@example.com')
+      expect(mockRedirect).toHaveBeenCalledWith('/login?reqId=818773500000&error=Could not authenticate user: Test error')
    })
 
    it('show email sent message on signIn success', async () => {
        
-      const formData = new FormData()
-      formData.append('email', 'test@example.com')
-
-      await signIn(formData)
-      expect(mockRedirect).toHaveBeenCalledWith('/login?message=Check email to continue sign in process')
+      await signIn('test@example.com')
+      expect(mockRedirect).toHaveBeenCalledWith('/login?reqId=818773500000&message=Check email to continue sign in process')
    })
 
    it('redirects to login page on signOut', async () => {
        
       await signOut()
-
       expect(mockRedirect).toHaveBeenCalledWith('/')
    })
 })
